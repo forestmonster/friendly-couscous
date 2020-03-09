179f04443da1e005f1a3e48bcb31163592bf49ff
Forest Monsen <forest.monsen@gmail.com>
2020-03-08 22:02:12 -0700

    Use switch statement to handle various routes.

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,17 @@ const http = require("http")
 const port = process.env.PORT || 3000
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" })
-  response.end("Hello world!")
+  // Lower case, remove query string, and training slash from request URI.
+  const path = request.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch(path) {
+    case '':
+      response.writeHead(200, { 'Content-Type': 'text/plain' })
+      response.end('Homepage')
+      break
+    case '/about':
+      // TODO
+      break
+  }
 })
 
 server.listen(port, () =>
