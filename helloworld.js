bc501a0ae4b2dc66462bd4210b0dafc00503f38c
Forest Monsen <forest.monsen@gmail.com>
2020-03-09 22:15:04 -0700

    Add default and about routes.

--- helloworld.js
+++ helloworld.js
@@ -3,14 +3,19 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer((request, response) => {
   // Lower case, remove query string, and training slash from request URI.
-  const path = request.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-  switch(path) {
-    case '':
-      response.writeHead(200, { 'Content-Type': 'text/plain' })
-      response.end('Homepage')
+  const path = request.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase()
+  switch (path) {
+    case "":
+      response.writeHead(200, { "Content-Type": "text/plain" })
+      response.end("Homepage")
       break
-    case '/about':
-      // TODO
+    case "/about":
+      response.writeHead(200, { "content-Type": "text/plain" })
+      response.end("About")
+      break
+    default:
+      response.writeHead(404, { "content-Type": "text/plain" })
+      response.end("Not Found")
       break
   }
 })
